13b759fe3ba3ceef60be45586e80f1d00dbf12af
Przmek Wójcik <przemek.wojcik@gmail.com>
2018-08-28 22:33:44 +0200

    first commit

---
+++ src/main/java/Zadania_6_9_08/App.java
@@ -0,0 +1,13 @@
+package Zadania_6_9_08;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
